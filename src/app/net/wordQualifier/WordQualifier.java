52a123da4dc9d92ab2e6016d02014e24ab1ccf19
the-albertox <126242013+The-Albertox@users.noreply.github.com>
2024-05-07 18:12:53 +0100

    feat(wordQualifier): added method addWord

--- src/app/net/wordQualifier/WordQualifier.java
+++ src/app/net/wordQualifier/WordQualifier.java
@@ -1,8 +1,10 @@
 package app.net.wordQualifier;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class WordQualifier {
     private Map<Character, List<String>> wordByInitial;
@@ -19,4 +21,24 @@ public class WordQualifier {
         this.wordByInitial = wordByInitial;
     }
 
+    public void addWord(String word) {
+        String sanitizedWord = sanitizeWord(word);
+        char initial = sanitizedWord.charAt(0);
+        if (!wordByInitial.containsKey(initial)) {
+            wordByInitial.put(initial, new ArrayList<>());
+        }
+        wordByInitial.get(initial).add(sanitizedWord);
+    }
+
+    private String sanitizeWord(String word) {
+        return word.toLowerCase().trim();
+    }
+
+    public Set<Character> getInitials() {
+        return wordByInitial.keySet();
+    }
+
+    public List<String> getWordsByInitial(char initial) {
+        return wordByInitial.getOrDefault(initial, new ArrayList<String>());
+    }
 }
