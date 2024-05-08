bf3ade318d177cec7446c0f6f2a758272db0f891
the-albertox <126242013+The-Albertox@users.noreply.github.com>
2024-05-08 12:43:53 +0100

    feat(menu): added first method

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
+import javax.swing.JOptionPane;
+
 import app.net.resource.Menu;
+import app.net.wordQualifier.WordQualifier;
 
 public class App {
+    static final WordQualifier qualifier = new WordQualifier();
+
     public static void main(String[] args) throws Exception {
         boolean exist = true;
         String option = "";
@@ -10,9 +15,19 @@ public class App {
             option = menu.getMenu();
             switch (option) {
                 case "1":
-
+                AddWord();
                     break;
             }
         }
     }
-}
+
+    public static void AddWord() {
+        
+            String wordToAdd = JOptionPane.showInputDialog(null, "introduzca la palabra");
+            qualifier.addWord(wordToAdd);
+            JOptionPane.showMessageDialog(null, "la palabra se ha agregado correctamente");
+        } 
+
+    }
+
+
