6b2e05936ad536eb84273decc962ee1d67404382
the-albertox <126242013+The-Albertox@users.noreply.github.com>
2024-05-08 12:30:09 +0100

    feat(menu): create menu that returns a menu

--- src/app/net/resource/Menu.java
+++ src/app/net/resource/Menu.java
@@ -1,6 +1,18 @@
 package app.net.resource;
 
+import javax.swing.JOptionPane;
 
 public class Menu {
-
+    public String getMenu() {
+        String Menu = """
+                Menu de opciones:
+                1. Añiadir palabra.
+                2. Eliminar palabra.
+                3.Existe palabra.
+                4. Mostar iniciales disponibles.
+                5. Ver palabras por inicial.
+                6. Cerrar programa
+                """;
+        return JOptionPane.showInputDialog(null, Menu, Menu, JOptionPane.INFORMATION_MESSAGE);
+    }
 }
