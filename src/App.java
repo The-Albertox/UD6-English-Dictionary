c2f5855e24079b4989bb0dd9a53cfaab9c06a6bf
the-albertox <126242013+The-Albertox@users.noreply.github.com>
2024-05-08 12:41:19 +0100

    feat(menu): initalize menu and the swith case

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import app.net.resource.Menu;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean exist = true;
+        String option = "";
+
+        Menu menu = new Menu();
+        while (exist) {
+            option = menu.getMenu();
+            switch (option) {
+                case "1":
+
+                    break;
+            }
+        }
     }
 }
